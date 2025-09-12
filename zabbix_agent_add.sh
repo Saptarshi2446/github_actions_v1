eeacf26804dd1ab8de2ca7afa5ea449309890937
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2025-09-12 12:53:28 +0530

    server value updated

--- zabbix_agent_add.sh
+++ zabbix_agent_add.sh
@@ -18,15 +18,15 @@ fi
 # Configure agentd.conf safely
 CONF="/etc/zabbix/zabbix_agentd.conf"
 
-sudo sed -i 's/^Server=.*/Server=127.0.0.1/' $CONF
-sudo sed -i 's/^ServerActive=.*/ServerActive=127.0.0.1/' $CONF
+sudo sed -i 's/^Server=.*/Server=3.15.199.235/' $CONF
+sudo sed -i 's/^ServerActive=.*/ServerActive=3.15.199.235/' $CONF
 sudo sed -i '/^Hostname=/s/^/#/' $CONF
 sudo sed -i 's/^ListenPort=.*/ListenPort=10050/' $CONF
 sudo sed -i '/^HostMetadata=/s/^/#/' $CONF
 
 # Add HostMetadata if not already present
 if ! grep -q "^HostMetadata=linux" $CONF; then
-    echo "HostMetadata=linux" | sudo tee -a $CONF
+    echo "HostMetadata=linux_github_actions" | sudo tee -a $CONF
 fi
 
 # Restart and enable service
