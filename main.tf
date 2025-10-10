ceb479c86a435161e8c46e78314e76f07bb5e72c
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2025-10-10 16:39:35 +0530

    terraform main file

---
+++ main.tf
@@ -0,0 +1,14 @@
+# main.tf
+provider "aws" {
+  region = "us-east-2" # Replace with your desired region
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  count         = 1 # Number of EC2 instances to create
+  ami           = "ami-0d1b5a8c13042c939" # Replace with a valid AMI ID for your region
+  instance_type = "t3.micro"
+  key_name      = "zabbix_server2" # Replace with your EC2 key pair name
+  tags = {
+    Name = "MyEC2Instance-${count.index}"
+  }
+}
