86a872ebc2bcb885163ee8c61bbda90c8abad1e7
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2025-10-15 17:27:00 +0530

    terraform_file_updated

--- main.tf
+++ main.tf
@@ -3,11 +3,36 @@ provider "aws" {
   region = "us-east-2" # Replace with your desired region
 }
 
+resource "aws_security_group" "my_ec2_sg" {
+  name        = "my-ec2-sg"
+  description = "Allow all inbound and outbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"            # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"]   # Allows from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"            # All outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]   # To anywhere
+  }
+
+  tags = {
+    Name = "my-ec2-sg"
+  }
+}
+
 resource "aws_instance" "my_ec2_instance" {
   count         = 1 # Number of EC2 instances to create
   ami           = "ami-0d1b5a8c13042c939" # Replace with a valid AMI ID for your region
   instance_type = "t3.micro"
   key_name      = "zabbix_server2" # Replace with your EC2 key pair name
+  vpc_security_group_ids = [aws_security_group.my_ec2_sg.id] # <-- Attach SG here  
   tags = {
     Name = "MyEC2Instance-${count.index}"
   }
