c5d634c7957e31ed65ae4f94db5eb4ad824d28ee
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2025-10-15 16:09:04 +0530

    ip-host issue fix v1

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2_instance" {
-  count         = 3 # Number of EC2 instances to create
+  count         = 1 # Number of EC2 instances to create
   ami           = "ami-0d1b5a8c13042c939" # Replace with a valid AMI ID for your region
   instance_type = "t3.micro"
   key_name      = "zabbix_server2" # Replace with your EC2 key pair name
