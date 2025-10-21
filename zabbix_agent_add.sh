269a77f79a16a2c78922889e302e8b374d24c0a2
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2025-10-21 12:39:08 +0530

    scripts updated

--- zabbix_agent_add.sh
+++ zabbix_agent_add.sh
@@ -9,7 +9,7 @@ if command -v zabbix_agentd >/dev/null 2>&1; then
 else
     echo "Installing Zabbix agent..."
     # Add repo
-    wget https://repo.zabbix.com/zabbix/6.0/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_6.0+ubuntu22.04_all.deb
+    wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_6.0+ubuntu22.04_all.deb 
     sudo dpkg -i zabbix-release_latest_6.0+ubuntu22.04_all.deb
     sudo apt update -y
     sudo apt install -y zabbix-agent
@@ -18,8 +18,8 @@ fi
 # Configure agentd.conf safely
 CONF="/etc/zabbix/zabbix_agentd.conf"
 
-sudo sed -i 's/^Server=.*/Server=18.216.51.112/' $CONF
-sudo sed -i 's/^ServerActive=.*/ServerActive=18.216.51.112/' $CONF
+sudo sed -i 's/^Server=.*/Server=18.216.190.95/' $CONF
+sudo sed -i 's/^ServerActive=.*/ServerActive=18.216.190.95/' $CONF
 sudo sed -i '/^Hostname=/s/^/#/' $CONF
 sudo sed -i 's/^ListenPort=.*/ListenPort=10050/' $CONF
 sudo sed -i 's/^#*\s*HostMetadata=.*/HostMetadata=linux_github_actions/' "/etc/zabbix/zabbix_agentd.conf"
--- zabbix_host_add.py
+++ zabbix_host_add.py
@@ -6,7 +6,7 @@ import sys
 custom_text = sys.argv[2]
 custom_IP = sys.argv[1]
 
-url = "http://18.216.51.112/zabbix/api_jsonrpc.php"
+url = "http://18.216.190.95/zabbix/api_jsonrpc.php"
 username = "Admin"
 password = "zabbix"
 
