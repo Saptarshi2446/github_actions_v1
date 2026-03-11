f7274285f0715ec56e3f01f61354b7160f4584a7
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2026-03-11 16:20:40 +0530

    zabbix_agent_add_sh_fixed

--- agent_install/zabbix_agent_add.sh
+++ agent_install/zabbix_agent_add.sh
@@ -1,8 +1,42 @@
 
+# #!/bin/bash
+# echo "Installing Zabbix agent"
+# sudo apt update
+# sudo apt install zabbix-agent -y
+# sudo systemctl enable zabbix-agent
+# sudo systemctl start zabbix-agent
+# echo "Agent installed successfully"
 #!/bin/bash
-echo "Installing Zabbix agent"
-sudo apt update
-sudo apt install zabbix-agent -y
+
+# Exit on error
+set -e
+
+# Check if zabbix agent is installed
+if command -v zabbix_agentd >/dev/null 2>&1; then
+    echo "Zabbix agent already installed"
+else
+    echo "Installing Zabbix agent..."
+    # Add and setup repo
+    wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_6.0+ubuntu24.04_all.deb 
+    sudo dpkg -i zabbix-release_latest_6.0+ubuntu24.04_all.deb
+    sudo apt update -y
+    sudo apt install -y zabbix-agent
+fi
+
+# Configure agentd.conf safely
+CONF="/etc/zabbix/zabbix_agentd.conf"
+
+sudo sed -i 's/^Server=.*/Server=172.31.33.57/' $CONF
+sudo sed -i 's/^ServerActive=.*/ServerActive=172.31.33.57/' $CONF
+sudo sed -i '/^Hostname=/s/^/#/' $CONF
+sudo sed -i 's/^ListenPort=.*/ListenPort=10050/' $CONF
+sudo sed -i 's/^#*\s*HostMetadata=.*/HostMetadata=linux_github_actions/' "/etc/zabbix/zabbix_agentd.conf"
+
+
+
+# Restart and enable service
 sudo systemctl enable zabbix-agent
-sudo systemctl start zabbix-agent
-echo "Agent installed successfully"
+sudo systemctl restart zabbix-agent
+sudo systemctl show -p ActiveState,SubState zabbix-agent
+
+echo "Zabbix agent installation and configuration complete."
